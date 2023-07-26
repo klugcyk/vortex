db179fc533896c562b5fc16ea7038bd2709d7f9c
klug <x1021122970@outlook.com>
2023-07-26 14:13:48 +0800

    add string operate

---
+++ string_operate/string_operate.cpp
@@ -0,0 +1,24 @@
+/*
+    文件等级：密一
+    author:klug
+    献给美人儿蕾梅黛丝
+    start:230726
+    last:230726
+*/
+
+#include "string_operate/string_operate.hpp"
+
+namespace meinString
+{
+
+int char_find(std::string word,char findWord)
+{
+    return word.find(findWord,1);
+}
+
+int char_find(std::string word,char findWord,int offset)
+{
+    return word.find(findWord,offset);
+}
+
+};
---
+++ string_operate/string_operate.hpp
@@ -0,0 +1,22 @@
+/*
+    文件等级：密一
+    author:klug
+    献给美人儿蕾梅黛丝
+    start:230726
+    last:230726
+*/
+#ifndef string_operate_hpp
+#define string_operate_hpp
+
+#include <string>
+#include <iostream>
+
+namespace meinString
+{
+
+int char_find(std::string word,char findWord);
+int char_find(std::string word,char findWord,int offset);
+
+};
+
+#endif //string_operate_hpp
